01b2b6887df05eaa7c98e669bffa00d47ffd910b
Torea <p.torea@icloud.com>
2025-03-13 23:09:31 +0400

    refactor: home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,73 +6,13 @@ import Button from '@/components/ui/Button';
 import { ErrorMessage } from '@/constants/ErrorMessage';
 import { postSolution } from '@/api/solutions';
 import { styles } from '@/styles/screens/home';
-
-interface PuzzleCell {
-  value: string | null;
-  inputName?: string | null;
-}
-
-type PuzzleNumbers = number[];
-
-const EMPTY_PUZZLE_GRID: PuzzleCell[][] = [
-  [
-    { value: '', inputName: 'A' },
-    { value: null },
-    { value: '', inputName: 'E' },
-    { value: '-' },
-    { value: '', inputName: 'F' },
-    { value: null },
-    { value: '66' },
-  ],
-  [
-    { value: '+' },
-    { value: null },
-    { value: '*' },
-    { value: null },
-    { value: '-' },
-    { value: null },
-    { value: '=' },
-  ],
-  [
-    { value: '13' },
-    { value: null },
-    { value: '12' },
-    { value: null },
-    { value: '11' },
-    { value: null },
-    { value: '10' },
-  ],
-  [
-    { value: '*' },
-    { value: null },
-    { value: '+' },
-    { value: null },
-    { value: '+' },
-    { value: null },
-    { value: '-' },
-  ],
-  [
-    { value: '', inputName: 'B' },
-    { value: null },
-    { value: '', inputName: 'D' },
-    { value: null },
-    { value: '', inputName: 'G' },
-    { value: null },
-    { value: '', inputName: 'I' },
-  ],
-  [
-    { value: '/' },
-    { value: '', inputName: 'C' },
-    { value: '+' },
-    { value: null },
-    { value: '*' },
-    { value: '', inputName: 'H' },
-    { value: '/' },
-  ],
-];
-
-const EMPTY_PUZZLE_NUMBERS = Array(9).fill(0);
-const PUZZLE_INPUT_POSITIONS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+import {
+  PuzzleCell,
+  EMPTY_PUZZLE_GRID,
+  EMPTY_PUZZLE_NUMBERS,
+  PUZZLE_INPUT_POSITIONS,
+  PuzzleNumbers,
+} from '@/constants/Puzzle';
 
 export default function PuzzleScreen() {
   const [puzzleGrid, setPuzzleGrid] =
---
+++ constants/Puzzle.ts
@@ -0,0 +1,74 @@
+export interface PuzzleCell {
+  value: string | null;
+  inputName?: string | null;
+}
+export const EMPTY_PUZZLE_NUMBERS = Array(9).fill(0);
+export const PUZZLE_INPUT_POSITIONS = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+];
+
+export type PuzzleNumbers = number[];
+
+export const EMPTY_PUZZLE_GRID: PuzzleCell[][] = [
+  [
+    { value: '', inputName: 'A' },
+    { value: null },
+    { value: '', inputName: 'E' },
+    { value: '-' },
+    { value: '', inputName: 'F' },
+    { value: null },
+    { value: '66' },
+  ],
+  [
+    { value: '+' },
+    { value: null },
+    { value: '*' },
+    { value: null },
+    { value: '-' },
+    { value: null },
+    { value: '=' },
+  ],
+  [
+    { value: '13' },
+    { value: null },
+    { value: '12' },
+    { value: null },
+    { value: '11' },
+    { value: null },
+    { value: '10' },
+  ],
+  [
+    { value: '*' },
+    { value: null },
+    { value: '+' },
+    { value: null },
+    { value: '+' },
+    { value: null },
+    { value: '-' },
+  ],
+  [
+    { value: '', inputName: 'B' },
+    { value: null },
+    { value: '', inputName: 'D' },
+    { value: null },
+    { value: '', inputName: 'G' },
+    { value: null },
+    { value: '', inputName: 'I' },
+  ],
+  [
+    { value: '/' },
+    { value: '', inputName: 'C' },
+    { value: '+' },
+    { value: null },
+    { value: '*', inputName: 'H' },
+    { value: '/' },
+  ],
+];
